c74f65885e3877da862bf3499fda6fb8193e8300
G6EJD - David <d.l.bird@sky.com>
2025-09-20 20:58:07 +0100

    Update ESP_METAR_DECODER_REVISED_NWS_FORMAT_V7_SSL.ino

--- ESP_METAR_DECODER_REVISED_NWS_FORMAT_V7_SSL.ino
+++ ESP_METAR_DECODER_REVISED_NWS_FORMAT_V7_SSL.ino
@@ -108,11 +108,10 @@ void GET_METAR(String station, String Name) { //client function to send/receive 
   const int time_delay = 20000;
   display_item(35, 100, "Decoding METAR", GREEN, 3);
   display_item(90, 135, "for " + station, GREEN, 3);
-  // http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=EGLL&hoursBeforeNow=1 (example)
-  // https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=EGLL&hoursBeforeNow=1
-  // ftp://tgftp.nws.noaa.gov/data/observations/metar/decoded/EGCC.TXT
-  String uri = String(host) + "/cgi-bin/data/metar.php?dataSource=metars&requestType=retrieve&format=xml&ids=" + station + "&hoursBeforeNow=1";
-  //String uri = String(host) + "/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=" + station;
+  // https://aviationweather.gov/api/data/stationinfo?ids=EGLL
+  // https://aviationweather.gov/api/data/metar?format=xml&hours=0&ids=EGLL&hoursBeforeNow=1
+  const char* host = "https://aviationweather.gov";
+  String uri = String(host) + "/api/data/metar?format=xml&ids=" + station + "&hoursBeforeNow=1";
   Serial.println("Connected, Requesting data for : " + Name);
   HTTPClient http;
   http.begin(uri.c_str());   // Specify the URL and maybe a certificate
@@ -893,3 +892,4 @@ void display_progress (String title, int percent) {
   <elevation_m>124.0</elevation_m>
   </METAR>
 */
+
